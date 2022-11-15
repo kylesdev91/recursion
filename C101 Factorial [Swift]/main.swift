b1d9a17bfeab0d93e9e32e6225963b3cc6e8bc8c
Kyle Hernandez <kyle.hernandez@student.allenisd.org>
2022-11-15 10:08:55 -0600

    commit for mr ben

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,29 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+var factorialNumber = n
+var newNumber = 1
+
+repeat {
+    print("Calculating factorial(\(factorialNumber))...")
+    factorialNumber -= 1
+} while (factorialNumber >= 1)
+print("Terminal case, returning 1.")
+
+factorialNumber = 2
+
+if n > 1 {
+    while factorialNumber <= n {
+        newNumber *= factorialNumber
+        print("Non-terminal case, returning \(newNumber).")
+        factorialNumber += 1
+    }
+}
+print("The final result is: \(newNumber).")
---
+++ C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,30 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 5
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+var factorialNumber = n
+var newNumber = 1
+
+repeat {
+    print("Calculating fibonacci(\(factorialNumber))...")
+    factorialNumber -= 1
+} while (factorialNumber >= 1)
+print("Terminal case, returning 1.")
+
+
+factorialNumber = 2
+
+if n > 1 {
+    while factorialNumber <= n {
+        newNumber *= factorialNumber
+        print("Non-terminal case, returning \(newNumber).")
+        factorialNumber += 1
+    }
+}
+print("The final result is: \(newNumber).")
